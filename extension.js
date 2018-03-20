09c3f439542a741da39ade869bebba4d53cafa04
WilliamDASILVA <william.da.silva@outlook.com>
2018-03-20 11:37:03 +0100

    Getting started with lottie view extension

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.lottieView', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
